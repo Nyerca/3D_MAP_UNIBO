c7f3ddaf8a98cdae64ca99aa5b36e8b778045989
Nyerca <facciani.e@gmail.com>
2018-09-10 15:00:13 +0200

    Fully add multiple sensors view, fully complete sensor avg historicals

--- Interactive3DMallMap/public/js/index_qual_stor_10.js
+++ Interactive3DMallMap/public/js/index_qual_stor_10.js
@@ -6,297 +6,155 @@
 	var qin_day3_graph3 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
 	var qin_day4_graph3 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
 	var qin_day5_graph3 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
-	var qin_day6_graph3 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+	var qin_day6_graph3 = [20,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
 
-	$(document).ready(function() {
+   	$(document).ready(function() {
 
 /* 
 	On receiving new values from the node file 
-		those values are inserted in the correspondents variables
-		the chart is after that re-built up.
-
-*/
-		socket.on('temp_storico',function(data) {	
-
+		the current value is inserted in the variables
+		the charts realtime values are then updated
+*/	
+		socket.on('temp_storico',function(data) {
 			var valori = data.qual_avg_val.split(';');
 			for(var field in valori) {
 				var line = valori[field].split('***');
 				
-				
+				var d = new Date("" + line[1]);
+				var str = $.datepicker.formatDate('yy-mm-dd', d);
+				if(str === day0) {
+					qin_day0_graph3[line[2]] = parseFloat(line[0]).toFixed(2);
+				} else if(str == day1) {
+					qin_day1_graph3[line[2]] = parseFloat(line[0]).toFixed(2);
+				} else if(str == day2) {
+					qin_day2_graph3[line[2]] = parseFloat(line[0]).toFixed(2);
+				} else if(str == day3) {
+					qin_day3_graph3[line[2]] = parseFloat(line[0]).toFixed(2);
+				} else if(str == day4) {
+					qin_day4_graph3[line[2]] = parseFloat(line[0]).toFixed(2);
+				} else if(str == day5) {
+					qin_day5_graph3[line[2]] = parseFloat(line[0]).toFixed(2);
+				} else if(str == day6) {
+					qin_day6_graph3[line[2]] = parseFloat(line[0]).toFixed(2);
+				}
 			}
-
 		
-		
-		
-// Chart Configuration
-			var myConfig = {
-				"globals":{
-					"font-family":"Roboto",
-				},
-				"graphset":[
-					{
-						"type":"piano",
-						"theme":"classic",
-						'zooming':false,
-						"title":{
-							"text":"Inquinamento dell'aria",
-							"background-color":"none",
-							"font-color":"white",
-							"font-size":"24px",
-							"adjust-layout":true,
-							"padding-bottom":25
-						},
-						"subtitle":{
-							"y":"38.5px",
-							"x":"-9.5px",
-							"text":"Valori medi di inquinamento settimanali",
-							"background-color":"none",
-							"font-color":"white",
-							"font-size":"14px",
-							"height":"25px"
-						},
-						"backgroundColor":"#3d3d3d",
-						"plotarea":{
-							"background-color": 'rgba(45, 45, 45, 1.0)'
-						},
-						"scaleX":{
-							"zooming":"false",
-							"placement":"opposite",
-							"lineWidth":0,
-							"item":{
-								"border-color":"none",
-								"size":"13px",
-								"font-color":"white"
-							},
-							"guide":{
-								"visible":false
-							},
-							"tick":{
-								"visible":false
-							},
-							"values":["0","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23"],
-						},
-						"scroll-x": {
-							"bar":{
-								"border-radius":3,
-								"background-color":"white",
-								"alpha":.5
-							},
-							"handle":{
-								"border-radius":5,
-								"background-color":"#01579B",
-								"border-top":"none",
-								"border-right":"none",
-								"border-bottom":"none",
-								"border-left":"none"
-							}
-						},
-						gui:{
-							behaviors:[ //default contextMenu behaviors
-								{
-									id: "Reload", //built-in id
-									enabled:"none" //sets visibility to show 
-								},
-								{
-									id: "SaveAsImage",
-									enabled:"none"
-								},
-								{
-									id: "DownloadPDF", //built-in id
-									enabled: "none" //sets visibility to show
-								},
-								{
-									id: "DownloadSVG",
-									enabled: "none"
-								},
-								{
-									id: "Print", 
-									enabled: "none"
-								},
-								{
-									id: "ViewSource", //built-in id 
-									enabled: "none" //sets visibility to hide
-								},
-								{
-									id: "HideGuide", //built-in id 
-									enabled: "none" //sets visibility to hide
-							}
-							] 
-						},
-						"scroll-y": {
-							"bar":{
-								"border-radius":3,
-								"background-color":"white",
-								"alpha":.5
-							},
-							"handle":{
-								"border-radius":5,
-								"background-color":"#01579B",
-								"border-top":"none",
-								"border-right":"none",
-								"border-bottom":"none",
-								"border-left":"none"
-							}
-						},
-						"scaleY":{
-							"lineWidth":0,
-							"mirrored":true,
-							"tick":{
-								"visible":false
-							},
-							"guide":{
-								"visible":false
-							},
-							"item":{
-								"border-color":"none",
-								"size":"13px",
-								"font-color":"white"
-							},
-							"values":["Mo","Tu","We","Th","Fr","Sa","Su"]
-						},
-						"legend":{
-							"layout":"x6",
-							"width":"80%",
-							"shadow":false,
-							"border-width":0,
-							"align":"center",
-							"vertical-align":"bottom",
-							"toggle-action":"none",
-							"backgroundColor":"#3d3d3d",
-							"item":{
-								"border-color":"none",
-								"size":"13px",
-								"font-color":"white"
-							},
-							"marker":{
-								"type":"square",
-								"border-radius":"8",
-								"border-color":"none",
-								"size":"13px"
-							},
-							"footer":{
-								"border-color":"none",
-								"background-color":"none",
-								"text-align":"center",
-								"font-size":"14px",
-								"font-color":"white"
-							}
-						},
-						"plot":{
-							"aspect":"none",
-							"borderWidth":2,
-							"borderColor":"#eeeeee",
-							"borderRadius":7,
-							"tooltip":{
-								"font-size":"14px",
-								"font-color":"white",
-								"text":" La qualità è: %v.",
-								"text-align":"left"
-							},
-							"rules":[
-								{
-									"rule":"%node-value >= 6",
-									"backgroundColor":"#2A0000",
-									"font-color":"#05636c"
-								},
-								{
-									"rule":"%node-value > 4 && %node-value <= 6",
-									"backgroundColor":"#460303",
-									"font-color":"#05636c"
-								},
-								{
-									"rule":"%node-value > 3 && %node-value <= 4",
-									"backgroundColor":"#6B0000",
-									"font-color":"#05636c"
-								},
-								{
-									"rule":"%node-value > 2 && %node-value <= 3",
-									"backgroundColor":"#910000",
-									"font-color":"#05636c"
-								},
-								{
-									"rule":"%node-value > 1 && %node-value <= 2",
-									"backgroundColor":"#D41C1C",
-									"font-color":"#05636c"
-								},
-								{
-									"rule":"%node-value > 0 && %node-value <= 1",
-									"backgroundColor":"#E34848",
-									"font-color":"#05636c"
-								},
-								{
-									"rule":"%node-value == 0",
-									"backgroundColor":"#FF9A9A",
-									"font-color":"#05636c"
-								},
-							]
-						},
-						"series":[
-							{
-								"values":qin_day0_graph3,
-								"text":"0-1",
-								"legend-marker":{
-									"backgroundColor":"#E34848",
-								}
-							},
-							{
-								"values":qin_day1_graph3,
-								"text":"1-2",
-								"legend-marker":{
-									"backgroundColor":"#D41C1C"
-								}
-							},
-							{
-								"values":qin_day2_graph3,
-								"text":"2-3",
-								"legend-marker":{
-									"backgroundColor":"#910000"
-								}
-							},
-							{
-								"values":qin_day3_graph3,
-								"text":"3-4",
-								"legend-marker":{
-									"backgroundColor":"#6B0000"
-								}
-							},
-							{
-								"values":qin_day4_graph3,
-								"text":"5-6",
-								"legend-marker":{
-									"backgroundColor":"#460303"
-								}
-							},
-							{
-								"values":qin_day5_graph3,
-								"text":">6",
-								"legend-marker":{
-									"backgroundColor":"#2A0000"
-								}
-							},
-							{
-								"values":qin_day6_graph3,
-								"text":"1-2 Ft",
-								"legend-marker":{
-									"backgroundColor":"#fff"
-								},
-								"legend-item":{
-									"visible":false
-								}
-							}
-						]
-					}
-				]
-			};
-/* 
-	Zingchart library used to create the chart.
+		/* 
+	highcharts library used to create the chart.
 */	
-			zingchart.render({ 
-				id : 'myChart_q10_storico', 
-				data : myConfig, 
-				height: 400, 
-				width: '100%' 
-			});
+
+
+		var min_graphh = 0;
+		var max_graph = 0;
+
+		$('#myChart_q10_storico').highcharts({
+chart: {
+        type: 'heatmap',
+        marginTop: 40,
+        marginBottom: 80,
+        plotBorderWidth: 1,
+		plotBackgroundColor: '#e4c7c6',
+		backgroundColor: '#e4c7c6',
+    },
+
+
+    title: {
+        text: "Qualità dell'aria media",
+		style: {
+         color: '#515158',
+      }
+    },
+
+    xAxis: {
+        categories: [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23],
+		style: {
+         color: '#515158',
+      }
+    },
+
+    yAxis: {
+        categories: [day6_graphLabel,
+					day5_graphLabel,
+					day4_graphLabel,
+					day3_graphLabel,
+					day2_graphLabel,
+					day1_graphLabel,
+					day0_graphLabel],
+        title: null,
+		style: {
+         color: '#515158',
+      }
+    },
+
+    
+
+  colorAxis: {
+    stops: [
+      [0, '#3060cf'],
+      [0.5, '#fffbbc'],
+      [0.9, '#c4463a']
+    ],
+	min: -5,
+	max: 30
+
+  },
+
+    legend: {
+        align: 'right',
+        layout: 'vertical',
+        margin: 0,
+        verticalAlign: 'top',
+        y: 25,
+        symbolHeight: 280
+    },
+
+    tooltip: {
+        formatter: function () {
+            return 'Quantità: <b>' +
+                this.point.value + '</b><br><b>' + this.series.yAxis.categories[this.point.y] + '</b> ore: <b>' + this.series.xAxis.categories[this.point.x] + ':00</b>';
+        }
+    },
+
+    series: [{
+        name: 'Sales per employee',
+        borderWidth: 1,
+        data: [
+		[0, 0, qin_day6_graph3[0]], [0, 1, qin_day5_graph3[0]], [0, 2, qin_day4_graph3[0]], [0, 3, qin_day3_graph3[0]], [0, 4, qin_day2_graph3[0]], [0, 5, qin_day1_graph3[0]], [0, 6, qin_day0_graph3[0]], 
+		[1, 0, qin_day6_graph3[1]], [1, 1, qin_day5_graph3[1]], [1, 2, qin_day4_graph3[1]], [1, 3, qin_day3_graph3[1]], [1, 4, qin_day2_graph3[1]], [1, 5, qin_day1_graph3[1]], [1, 6, qin_day0_graph3[1]], 
+		[2, 0, qin_day6_graph3[2]], [2, 1, qin_day5_graph3[2]], [2, 2, qin_day4_graph3[2]], [2, 3, qin_day3_graph3[2]], [2, 4, qin_day2_graph3[2]], [2, 5, qin_day1_graph3[2]], [2, 6, qin_day0_graph3[2]], 
+		[3, 0, qin_day6_graph3[3]], [3, 1, qin_day5_graph3[3]], [3, 2, qin_day4_graph3[3]], [3, 3, qin_day3_graph3[3]], [3, 4, qin_day2_graph3[3]], [3, 5, qin_day1_graph3[3]], [3, 6, qin_day0_graph3[3]], 
+		
+		[4, 0, qin_day6_graph3[4]], [4, 1, qin_day5_graph3[4]], [4, 2, qin_day4_graph3[4]], [4, 3, qin_day3_graph3[4]], [4, 4, qin_day2_graph3[4]], [4, 5, qin_day1_graph3[4]], [4, 6, qin_day0_graph3[4]], 
+		[5, 0, qin_day6_graph3[5]], [5, 1, qin_day5_graph3[5]], [5, 2, qin_day4_graph3[1]], [5, 3, qin_day3_graph3[5]], [5, 4, qin_day2_graph3[5]], [5, 5, qin_day1_graph3[5]], [5, 6, qin_day0_graph3[5]], 
+		[6, 0, qin_day6_graph3[6]], [6, 1, qin_day5_graph3[6]], [6, 2, qin_day4_graph3[1]], [6, 3, qin_day3_graph3[6]], [6, 4, qin_day2_graph3[6]], [6, 5, qin_day1_graph3[6]], [6, 6, qin_day0_graph3[6]], 
+		[7, 0, qin_day6_graph3[7]], [7, 1, qin_day5_graph3[7]], [7, 2, qin_day4_graph3[1]], [7, 3, qin_day3_graph3[7]], [7, 4, qin_day2_graph3[7]], [7, 5, qin_day1_graph3[7]], [7, 6, qin_day0_graph3[7]], 
+		[8, 0, qin_day6_graph3[8]], [8, 1, qin_day5_graph3[8]], [8, 2, qin_day4_graph3[1]], [8, 3, qin_day3_graph3[8]], [8, 4, qin_day2_graph3[8]], [8, 5, qin_day1_graph3[8]], [8, 6, qin_day0_graph3[8]], 
+		[9, 0, qin_day6_graph3[9]], [9, 1, qin_day5_graph3[9]], [9, 2, qin_day4_graph3[1]], [9, 3, qin_day3_graph3[9]], [9, 4, qin_day2_graph3[9]], [9, 5, qin_day1_graph3[9]], [9, 6, qin_day0_graph3[9]], 
+		[10, 0, qin_day6_graph3[10]], [10, 1, qin_day5_graph3[10]], [10, 2, qin_day4_graph3[10]], [10, 3, qin_day3_graph3[10]], [10, 4, qin_day2_graph3[10]], [10, 5, qin_day1_graph3[10]], [10, 6, qin_day0_graph3[10]], 
+		[11, 0, qin_day6_graph3[11]], [11, 1, qin_day5_graph3[11]], [11, 2, qin_day4_graph3[11]], [11, 3, qin_day3_graph3[11]], [11, 4, qin_day2_graph3[11]], [11, 5, qin_day1_graph3[11]], [11, 6, qin_day0_graph3[11]], 
+		[12, 0, qin_day6_graph3[12]], [12, 1, qin_day5_graph3[12]], [12, 2, qin_day4_graph3[12]], [12, 3, qin_day3_graph3[12]], [12, 4, qin_day2_graph3[12]], [12, 5, qin_day1_graph3[12]], [12, 6, qin_day0_graph3[12]], 
+		[13, 0, qin_day6_graph3[13]], [13, 1, qin_day5_graph3[13]], [13, 2, qin_day4_graph3[13]], [13, 3, qin_day3_graph3[13]], [13, 4, qin_day2_graph3[13]], [13, 5, qin_day1_graph3[13]], [13, 6, qin_day0_graph3[13]], 
+		[14, 0, qin_day6_graph3[14]], [14, 1, qin_day5_graph3[14]], [14, 2, qin_day4_graph3[14]], [14, 3, qin_day3_graph3[14]], [14, 4, qin_day2_graph3[14]], [14, 5, qin_day1_graph3[14]], [14, 6, qin_day0_graph3[14]], 
+		[15, 0, qin_day6_graph3[15]], [15, 1, qin_day5_graph3[15]], [15, 2, qin_day4_graph3[15]], [15, 3, qin_day3_graph3[15]], [15, 4, qin_day2_graph3[15]], [15, 5, qin_day1_graph3[15]], [15, 6, qin_day0_graph3[15]], 
+		[16, 0, qin_day6_graph3[16]], [16, 1, qin_day5_graph3[16]], [16, 2, qin_day4_graph3[16]], [16, 3, qin_day3_graph3[16]], [16, 4, qin_day2_graph3[16]], [16, 5, qin_day1_graph3[16]], [16, 6, qin_day0_graph3[16]], 
+		[17, 0, qin_day6_graph3[17]], [17, 1, qin_day5_graph3[17]], [17, 2, qin_day4_graph3[17]], [17, 3, qin_day3_graph3[17]], [17, 4, qin_day2_graph3[17]], [17, 5, qin_day1_graph3[17]], [17, 6, qin_day0_graph3[17]], 
+		[18, 0, qin_day6_graph3[18]], [18, 1, qin_day5_graph3[18]], [18, 2, qin_day4_graph3[18]], [18, 3, qin_day3_graph3[18]], [18, 4, qin_day2_graph3[18]], [18, 5, qin_day1_graph3[18]], [18, 6, qin_day0_graph3[18]], 
+		[19, 0, qin_day6_graph3[19]], [19, 1, qin_day5_graph3[19]], [19, 2, qin_day4_graph3[19]], [19, 3, qin_day3_graph3[19]], [19, 4, qin_day2_graph3[19]], [19, 5, qin_day1_graph3[19]], [19, 6, qin_day0_graph3[19]], 
+		[20, 0, qin_day6_graph3[20]], [20, 1, qin_day5_graph3[20]], [20, 2, qin_day4_graph3[20]], [20, 3, qin_day3_graph3[20]], [20, 4, qin_day2_graph3[20]], [20, 5, qin_day1_graph3[20]], [20, 6, qin_day0_graph3[20]], 
+		[21, 0, qin_day6_graph3[21]], [21, 1, qin_day5_graph3[21]], [21, 2, qin_day4_graph3[21]], [21, 3, qin_day3_graph3[21]], [21, 4, qin_day2_graph3[21]], [21, 5, qin_day1_graph3[21]], [21, 6, qin_day0_graph3[21]], 
+		[22, 0, qin_day6_graph3[22]], [22, 1, qin_day5_graph3[22]], [22, 2, qin_day4_graph3[22]], [22, 3, qin_day3_graph3[22]], [22, 4, qin_day2_graph3[22]], [22, 5, qin_day1_graph3[22]], [22, 6, qin_day0_graph3[22]], 
+		[23, 0, qin_day6_graph3[23]], [23, 1, qin_day5_graph3[23]], [23, 2, qin_day4_graph3[23]], [23, 3, qin_day3_graph3[23]], [23, 4, qin_day2_graph3[23]], [23, 5, qin_day1_graph3[23]], [23, 6, qin_day0_graph3[23]], 
+		],
+        dataLabels: {
+            enabled: true,
+            color: '#000000'
+        }
+    }]
+
+	});
+	
 		});
+		
+
 	});
 	
